5d46ab7ac2f97170a29b8922332ca270621b299b
Anthony Ruiz <yobakaj@gmail.com>
2023-08-02 15:19:55 -0400

    testing library

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,64 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from 'react';
+import { MDBContainer, MDBRow, MDBCol,  MDBCard,
+  MDBNavbar,
+  MDBNavbarBrand,
+  MDBNavbarToggler,
+  MDBNavbarItem,
+  MDBNavbarLink,
+  MDBCollapse,
+  MDBBtn,
+  MDBNavbarNav,
+  MDBIcon,
+  MDBInputGroup,
+  MDBCardImage,
+  MDBCardBody,
+  MDBCardTitle,
+  MDBCardText,
+  MDBCardFooter } from 'mdb-react-ui-kit';
 
-function App() {
-  const [count, setCount] = useState(0)
+import "./App.css";
+
+export default function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
 
-export default App
+    <MDBContainer className='containerColor'>
+
+      <MDBContainer className='containerColor'>
+        <h1>ALL PLAYERS</h1>
+      </MDBContainer>
+
+      <MDBRow>
+        <MDBCol md='4'>
+        <MDBInputGroup tag="form" className='d-flex w-auto mb-3'>
+          <input className='form-control' placeholder="Type query" aria-label="Search" type='Search' />
+        </MDBInputGroup>
+        </MDBCol>
+      </MDBRow>
+
+      <MDBRow className='row-cols-1 row-cols-md-3 g-4'>
+        <MDBCol>
+          <MDBCard className='h-100'>
+            <MDBCardImage
+              src='https://mdbootstrap.com/img/new/standard/city/044.webp'
+              alt='...'
+              position='top'
+            />
+            <MDBCardBody className='cardBodyColor'>
+              <MDBCardTitle>Card title</MDBCardTitle>
+              <MDBCardText>
+                This is a longer card with supporting text below as a natural lead-in to additional content.
+                This content is a little bit longer.
+              </MDBCardText>
+            </MDBCardBody>
+            <MDBCardFooter>
+              <small className='text-muted'>Last updated 3 mins ago</small>
+            </MDBCardFooter>
+          </MDBCard>
+        </MDBCol>
+      </MDBRow>
+    </MDBContainer>
+    </>
+  );
+}
